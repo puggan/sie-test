b9895d3e5b8cc064e2e9819d85dbe6b68c88be12
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-03-20 00:52:58 +0100

    Test: save file

--- tests/Sie4Test.php
+++ tests/Sie4Test.php
@@ -7,9 +7,19 @@
 
 	final class Sie4Test extends TestCase
 	{
+		public $base_file = __DIR__ . '/files/Sie4.se';
+
 		public function testCreateFromFile(): void
 		{
-			$sie = Sie4::loadFile(__DIR__ . '/files/Sie4.se');
+			$sie = Sie4::loadFile($this->base_file);
 			$this->assertInstanceOf(Sie4::class, $sie);
+		}
+
+		public function testSave(): void
+		{
+			$tmp_file = tempnam(sys_get_temp_dir(), 'sie_');
+			$sie = Sie4::loadFile($this->base_file);
+			$sie->save($tmp_file);
+			$this->assertFileEquals($this->base_file, $tmp_file);
 		}
 	}
