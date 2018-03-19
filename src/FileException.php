d9e5a261585ea83f3ba2827b871358fcb64977c0
Thorbjörn Backlund <thorbjorn@spiro.se>
2018-03-20 00:30:04 +0100

    Exception: FileException

---
+++ src/FileException.php
@@ -0,0 +1,14 @@
+<?php
+
+	namespace Puggan\Sie;
+
+	/**
+	 * Class FileException
+	 * Exception regardning files
+	 *
+	 * @package Puggan\Sie
+	 */
+	class FileException extends \Exception
+	{
+
+	}
